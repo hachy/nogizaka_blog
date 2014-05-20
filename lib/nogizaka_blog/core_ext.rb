150211895ddf6cd3a02f93c3ec4d167cd11ad24a
hachy <1613863+hachy@users.noreply.github.com>
2014-05-20 17:46:41 +0900

    add to_kanji

--- lib/nogizaka_blog/core_ext.rb
+++ lib/nogizaka_blog/core_ext.rb
@@ -1,7 +1,15 @@
-module NogizakaBlogExtentions
-  refine Array do
-    def sum
-      map(&:to_i).inject(:+)
+module NogizakaBlog
+  module Extentions
+    refine String do
+      def to_kanji
+        MEMBER[self]
+      end
+    end
+
+    refine Array do
+      def sum
+        map(&:to_i).inject(:+)
+      end
     end
   end
 end
