a11b456f851fc0316de333b5b9fd916864475ab0
hachy <1613863+hachy@users.noreply.github.com>
2015-02-02 09:52:47 +0900

    add kenkyusei

--- lib/nogizaka_blog/member.rb
+++ lib/nogizaka_blog/member.rb
@@ -4,6 +4,7 @@ module NogizakaBlog
     'erika.ikuta' => '生田絵梨花',
     'rina.ikoma' => '生駒里奈',
     'karin.itou' => '伊藤かりん',
+    'junna.itou' => '伊藤 純奈',
     'marika.ito' => '伊藤万理華',
     'sayuri.inoue' => '井上小百合',
     'misa.eto' => '衛藤美彩',
@@ -15,9 +16,12 @@ module NogizakaBlog
     'yuuri.saito' => '斉藤優里',
     'iori.sagara' => '相楽伊織',
     'reika.sakurai' => '桜井玲香',
+    'kotoko.sasaki' => '佐々木琴子',
     'mai.shiraishi' => '白石麻衣',
     'mai.shinuchi' => '新内眞衣',
+    'ayane.suzuki' => '鈴木絢音',
     'kazumi.takayama' => '高山一実',
+    'ranze.terada' => '寺田蘭世',
     'kana.nakada' => '中田花奈',
     'himeka.nakamoto' => '中元日芽香',
     'seira.nagashima' => '永島聖羅',
@@ -30,7 +34,9 @@ module NogizakaBlog
     'minami.hoshino' => '星野みなみ',
     'miona.hori' => '堀未央奈',
     'sayuri.matsumura' => '松村沙友理',
+    'rena.yamazaki' => '山崎怜奈',
     'yumi.wakatsuki' => '若月佑美',
+    'miria.watanabe' => '渡辺みり愛',
     'maaya.wada' => '和田まあや'
   }.freeze
 end
--- lib/nogizaka_blog/version.rb
+++ lib/nogizaka_blog/version.rb
@@ -1,3 +1,3 @@
 module NogizakaBlog
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
